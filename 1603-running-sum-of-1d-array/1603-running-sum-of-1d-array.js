fe7a7fee62af0b00f9047b7cb1f9b3dc855767f8
chinchina lalu <chinchinalalu.kmm@gmail.com>
2025-08-28 12:56:16 +0530

    Time: 1 ms (41.83%), Space: 54.2 MB (74.77%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
@@ -3,14 +3,17 @@
  * @return {number[]}
  */
 var runningSum = function (nums) {
-    let num0 = new Array()
-    let sum = 0;
-    for (let i = 0; i < nums.length; i++) {
-        sum += nums[i]
-        num0.push(sum)
-
+    let num1=new Array()
+    let sum=0;
+    for(let n of nums){
+      sum+=n
+      num1.push(sum)
+    //   console.log(n)
     }
-    return num0;
 
-};
+
+    return num1;
+    }
+
+
 // console.log(runningSum([1, 2, 3, 4]))
